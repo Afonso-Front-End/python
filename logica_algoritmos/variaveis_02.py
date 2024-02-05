7e34a5512c432cb03e31b4cfdf64502bbce2fd46
Afonso-Front-End <afonsogithub@gmail.com>
2024-02-05 12:23:39 -0300

    Enviando estudo

---
+++ logica_algoritmos/variaveis_02.py
@@ -0,0 +1,31 @@
+# tipos de variaveis
+nome_usuario = "Afonso"
+
+media = 0  
+
+nome , idade = "Afonso", 24
+
+estado = True 
+
+# function type
+print(type(media))
+print(type(nome))
+print(type(estado))
+print(type(1+2j))
+
+# function isinstance
+a = 10
+b = 'sol'
+c =  40
+d = a * c
+# print(isinstance(a, int))
+# print(isinstance(b, str))
+# print(isinstance(a, float))
+# print(isinstance(a, (int, float)))
+print(d)
+
+
+
+
+
+
